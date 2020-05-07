6fbbf70a6aa6db33512e042758781f12d440ada1
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-05-07 13:54:26 +0200

    Added Billboarding script

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.flipX = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(PlayerController.Instance.transform.position, -Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    public float lifeTime = 13f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
